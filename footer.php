cbf888641373ab9ea7afbaa7c0255e08c1b937c0
Ada Gürsoy <50636585+ShinnAsukha@users.noreply.github.com>
2020-09-10 09:18:46 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div style="border: 0px solid; border-radius: 10px;" class="container">
+ 
+<footer style="border: 0px solid; border-radius: 10px;" class="copyright">
+<br>
+<p class="text-center"><b>&copy <b><?php echo $ayarcek['sunucu_isim']; ?></b> - Tüm Hakları Saklıdır!</b></p>
+
+<p style="font-size: 12px;" class="text-center"><a style="color: orange;" href="https://www.mc-tr.com/uyeler/ada06.103377/">Ada Gürsoy</a> arafından <a style="color: orange;" href="https://nacsshost.com">NacssWeb</a></p><br>
+</footer> 
+ 
+</div>
